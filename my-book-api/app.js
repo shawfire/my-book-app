8102ece069d7ae48e6fdfd95ecacffc183045e5b
John Shaw <shaw_fire@yahoo.com>
2017-06-19 15:38:05 +1000

    Refactored bookRoutes to it's own file.

--- my-book-api/app.js
+++ my-book-api/app.js
@@ -1,13 +1,13 @@
-var express = require('express'),
+const express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
 // Open connnect to the bookAPI database if it exists otherwise create the database.
-var db = mongoose.connect('mongodb://localhost/bookAPI');
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 
-var Book = require('./models/bookModel');
+const Book = require('./models/bookModel');
 
-var app = express();
+const app = express();
 
 // Get the port number from the ENV variable PORT otherwise set it to 3000
 var port = process.env.PORT || 3000;
@@ -17,42 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Routes
-var bookRouter = express.Router();
+bookRouter = require('./routes/bookRoutes')(Book);
 
-bookRouter.route("/Books")
-    .post((request, response) => {
-        var book = new Book(request.body);
-        book.save();
-        // 201 - Created Status
-        response.status(201).send(book);
-    })
-    .get((request, response) => {
-        var query = request.query;
-        var query = {};
-        if (request.query.genre) {
-            query.genre = request.query.genre;
-        }
-        Book.find(query, function(err, books) {
-            if (err) {
-                response.status(500).send(err);
-            } else {
-                response.json(books);
-            }
-        });
-    });
-
-bookRouter.route("/Books/:bookId")
-    .get((request, response) => {
-        Book.findById(request.params.bookId, (err, book) => {
-            if (err) {
-                response.status(500).send(err);
-            } else {
-                response.json(book);
-            }
-        });
-    });
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
 
 app.get('/', (request, response) => {
     response.send('Welcome to my API.....');
